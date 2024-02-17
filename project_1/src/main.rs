87aee80ae4d72f512e09fab52068673034caf036
PuncakeBanini <98001955+PuncakeBanini@users.noreply.github.com>
2024-02-16 20:10:59 -0700

    Error lines

--- project_1/src/main.rs
+++ project_1/src/main.rs
@@ -56,6 +56,30 @@ fn plot_stock_data(symbol: &str, data: &[(f64, f64, f64, f64, f64)]) -> Result<(
             )
         })
     )?;
+    chart.draw_series(volatile_data.iter().map(|x| {
+        Circle::new(
+            (x.0, x.1),
+            3,
+            &BLUE,
+        )
+    }))?;
+    let offset = 100000.0;
+    chart.draw_series(
+        volatile_data.iter().map(|x| {
+            PathElement::new(
+                vec![((x.0)-offset, x.2), ((x.0)+offset, x.2)],
+                &BLUE,
+            )
+        })
+    )?;
+    chart.draw_series(
+        volatile_data.iter().map(|x| {
+            PathElement::new(
+                vec![((x.0)-offset, x.3), ((x.0)+offset, x.3)],
+                &BLUE,
+            )
+        })
+    )?;
     Ok(())
 }
 
